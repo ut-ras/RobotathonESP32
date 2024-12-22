033c890718db7d713d2742e78d11a2726eb8af99
Toby Nguyen <tobyanguyen1@gmail.com>
2024-12-21 21:38:26 -0600

    Added Espressif APDS9950 (color sensor) and iot_servo components through ESP Component Registry (stored in managed components)

--- main/main.c
+++ main/main.c
@@ -7,7 +7,6 @@
 #include <btstack_run_loop.h>
 #include <btstack_stdio_esp32.h>
 #include <uni.h>
-
 #include "sdkconfig.h"
 
 // Sanity check
@@ -18,13 +17,82 @@
 // Defined in my_platform.c
 struct uni_platform* get_my_platform(void);
 
-void testlogi(const char *format, ...) {
+// NOT part of sample ========================================
+#include "freertos/FreeRTOS.h" 
+#include "freertos/task.h" 
+#include "apds9960.h" //https://github.com/espressif/esp-iot-solution/tree/23c164ec7f748957e41c2f39a6ed9a6d4b4af56a/components/sensors/gesture/apds9960
+// #include "iot_servo.h" // https://github.com/espressif/esp-iot-solution/tree/f5ca5b553873e8ff3c044d5bae56d0eae1d0673b/components/motor/servo
+
+
+// qtr line sensor 
+// GP2Y0A21YK0F sharp IR distance sensor
+
+
+// color sensor init
+// initColorSensor() {
+//     i2c_bus_handle_t i2c_bus = NULL;
+//     apds9960_handle_t apds9960 = NULL;
+
+//     //Step1: Init I2C bus and VL io
+    // gpio_config_t cfg;
+//     cfg.pin_bit_mask = BIT(APDS9960_VL_IO);
+//     cfg.intr_type = 0;
+//     cfg.mode = GPIO_MODE_OUTPUT;
+//     cfg.pull_down_en = 0;
+//     cfg.pull_up_en = 0;
+//     gpio_config(&cfg);
+//     gpio_set_level(APDS9960_VL_IO, 0);
+
+//     int i2c_master_port = APDS9960_I2C_MASTER_NUM;
+//     i2c_config_t conf = {
+//         .mode = I2C_MODE_MASTER,
+//         .sda_io_num = APDS9960_I2C_MASTER_SDA_IO,
+//         .sda_pullup_en = GPIO_PULLUP_ENABLE,
+//         .scl_io_num = APDS9960_I2C_MASTER_SCL_IO,
+//         .scl_pullup_en = GPIO_PULLUP_ENABLE,
+//         .master.clk_speed = APDS9960_I2C_MASTER_FREQ_HZ,
+//     };
+// i2c_bus = i2c_bus_create(i2c_master_port, &conf);
+
+// //Step2: Init apds9960
+// apds9960 = apds9960_create(i2c_bus, APDS9960_I2C_ADDRESS);
+
+// //Step3: Read gesture
+// uint8_t gesture = apds9960_read_gesture(apds9960);
+// }
+
+
+void serialPrint(const char *format, ...){ // implementation is probably bad lol
     va_list args;
     va_start(args, format);
     vfprintf(stdout, format, args); // Write formatted output to stdout
     va_end(args);
     fflush(stdout); // Ensure immediate output
 }
+
+void digitalWrite(int pin, int value) {
+    return;
+}
+
+void analogWrite(int pin, int value) {
+    return;
+}
+
+int digitalRead(int pin) {
+    return 1;
+}
+
+int analogRead(int pin) {
+    return 1;
+}
+
+void delay(int ms) {
+    vTaskDelay(ms / portTICK_PERIOD_MS);
+}
+
+
+// make controller data a global variable and have them access it asynchrnously(?)
+
 
 int app_main(void) {
     // hci_dump_open(NULL, HCI_DUMP_STDOUT);
@@ -54,7 +122,8 @@ int app_main(void) {
     int i = 0;
     while(1) {
         i++;
-        testlogi("Hello, World! %d\n", i);
+        serialPrint("Hello, World! %d\n", i);
+        logi("fuck"); // not sure how the buffers of logi is interacting with my serialPrint
     }
     // Does not return.
     btstack_run_loop_execute();
