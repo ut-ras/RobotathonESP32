5cf3c3f8d760c6230752216e7f3da5d55acfcf5b
unknown <tobyanguyen1@gmail.com>
2025-01-03 10:56:03 -0600

    added mentorbot test code

--- main/arduino_main.cpp
+++ main/arduino_main.cpp
@@ -3,18 +3,40 @@
 // http://retro.moe/unijoysticle2
 
 #include "sdkconfig.h"
+
 #include <Arduino.h>
 #include <Bluepad32.h>
-#include <uni.h>
 #include "controller_callbacks.h"
 
-extern ControllerPtr myControllers[BP32_MAX_GAMEPADS];
+#include <Arduino_APDS9960.h>
+#include <Wire.h>
+
+#include <QTRSensors.h>
+
+#include <ESP32Servo.h>
+#include <QTRSensors.h>
+#include <ESP32SharpIR.h>
+
+#define APDS9960_INT 2
+#define I2C_SDA 21
+#define I2C_SCL 22
+#define I2C_FREQ 100000
+
+ControllerPtr myControllers[BP32_MAX_GAMEPADS];
+
+TwoWire I2C_0 = TwoWire(0);
+APDS9960 apds = APDS9960(I2C_0, APDS9960_INT);
+
+QTRSensors qtr;
+uint16_t sensors[2];
+
+ESP32SharpIR IRSensorName(ESP32SharpIR::GP2Y0A21YK0F, 12);
 
 void dumpGamepad(ControllerPtr ctl) {
     Console.printf(
         "DPAD: %d A: %d B: %d X: %d Y: %d LX: %d LY: %d RX: %d RY: %d L1: %d R1: %d L2: %d R2: %d\n",
         ctl->dpad(),         // D-pad
-        ctl->a(),           // buttons
+        ctl->a(), 
         ctl->b(),
         ctl->x(),
         ctl->y(),
@@ -29,21 +51,95 @@ void dumpGamepad(ControllerPtr ctl) {
     );
 }
 
+// Arduino setup function. Runs in CPU 1
 void setup() {
     BP32.setup(&onConnectedController, &onDisconnectedController);
     BP32.forgetBluetoothKeys(); 
     esp_log_level_set("gpio", ESP_LOG_ERROR); // suppress info log spam from gpio_isr_service
-    uni_bt_allowlist_set_enabled(true);
 }
 
+// Arduino loop function. Runs in CPU 1.
 void loop() {
     vTaskDelay(1); // ensures WDT does not get triggered when no controller is connected
     BP32.update(); 
-    for (auto myController : myControllers) { // only execute code when controller is connected
+    for (auto myController : myControllers) {
         if (myController && myController->isConnected() && myController->hasData()) {        
-          
-            // Your code goes here!
-            dumpGamepad(myController); // prints the gamepad state, delete or comment if don't need
+
+            Console.print("Idle\n");
+            // while(1) {
+            //     BP32.update();
+            //     dumpGamepad(myController);
+            //     delay(100);
+            // }
+
+            if(myController->a()) {
+                Console.print("button a pressed - entering color mode\n");
+                int r, g, b, a;
+                //sets up color sensor
+                I2C_0.begin(I2C_SDA, I2C_SCL, I2C_FREQ);
+                // apds.setInterruptPin(APDS9960_INT); // do we need this?
+                apds.begin();
+                while(1) {
+                    BP32.update();
+                    // Wait until color is read from the sensor 
+                    int watchDogAtHome = 0;
+                    while (!apds.colorAvailable()) { 
+                        delay(5);
+                        watchDogAtHome++;
+                        if(watchDogAtHome > 1000) {
+                            Console.println("Color sensor broken lmao\n");
+                            break;
+                        }
+                    }
+                    apds.readColor(r, g, b, a);
+                    // Read color from sensor apds.readColor(r, g, b, a);
+                    // Print color in decimal 
+                    Console.printf("RED: %d GREEN: %d BLUE: %d AMBIENT: %d\n", r, g, b, a);
+                    delay(100);
+                    if(myController->b()) {
+                        Console.print("button b pressed - exiting to main\n");
+                        break;
+                    }
+                }
+            }
+            else if(myController->x()) {
+                Console.print("button x pressed - entering line mode\n");
+                // TODO figure out how to deinitialize these
+                qtr.setTypeAnalog(); // or setTypeAnalog()
+
+                const uint8_t pins[] = {33, 32};
+                const uint8_t numPins = 2;
+                qtr.setSensorPins(pins, numPins);
+                for (uint8_t i = 0; i < 250; i++) { 
+                    Console.println("calibrating");
+                    qtr.calibrate(); 
+                    delay(20);
+                }
+                while(1) {
+                    BP32.update();
+                    qtr.readLineBlack(sensors); // Get calibrated sensor values returned in the sensors array
+                    Console.printf("S1: %d S2: %d\n", sensors[0], sensors[1]);
+                    delay(50);
+                    if(myController->b()) {
+                        Console.println("button b pressed - exiting to main");
+                        break;
+                    }
+                }   
+            }
+            else if(myController->y()) {
+                Console.print("button y pressed - entering IR mode");
+                IRSensorName.setFilterRate(1.0f);
+                float distance;
+                while(1) {
+                    BP32.update();
+                    distance = IRSensorName.getDistanceFloat();
+                    Console.println(distance); 
+                    if(myController->b()) {
+                        Console.println("button b pressed - exiting to main");
+                        break;
+                    }
+                }   
+            }
         }
     }
 }
