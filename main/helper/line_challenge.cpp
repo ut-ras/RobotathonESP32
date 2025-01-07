ddbd43acd852564ff12520b03d2b5d3aed407f95
Toby Nguyen <tobyanguyen1@gmail.com>
2025-01-07 00:56:09 -0600

    maybe working line follow

--- main/helper/line_challenge.cpp
+++ main/helper/line_challenge.cpp
@@ -8,18 +8,21 @@
 #define LINE3_PIN 34
 #define LINE4_PIN 35
 
-#define NUM_SENSORS 4
+#define NUM_LINE_SENSORS 4
+#define THRESHOLD 500
 
 QTRSensors qtr;
-uint16_t sensors[NUM_SENSORS];
+uint16_t sensors[NUM_LINE_SENSORS];
 bool lineIsCalibrated = false; // false will trigger calibration + write to NVS, true will trigger read from NVS
 
-// note that NVS functionality will break NUM_SENSORS != 4
+// note that NVS functionality will break NUM_LINE_SENSORS != 4
 void lineChallenge(ControllerPtr myController) {
     qtr.setTypeAnalog();
 
     const uint8_t pins[] = {LINE1_PIN, LINE2_PIN, LINE3_PIN, LINE4_PIN};
-    const uint8_t numSensors = NUM_SENSORS;
+    const uint8_t numSensors = NUM_LINE_SENSORS;
+    bool boolSensorData[4];
+
     qtr.setSensorPins(pins, numSensors);
     if(!lineIsCalibrated) { // only calibrate if necessary. otherwise pull calbration data from flash
         pinMode(2, OUTPUT);
@@ -45,6 +48,31 @@ void lineChallenge(ControllerPtr myController) {
         qtr.readLineBlack(sensors); // Get calibrated sensor values returned in the sensors array
         Console.printf("S1: %d S2: %d S3: %d S4: %d\n", sensors[0], sensors[1], sensors[2], sensors[3]);
         delay(50);
+
+        // remap sensors to array of booleans for ease of typing
+        // false is no line
+        // true is line
+        for(int i = 0; i < NUM_LINE_SENSORS; i++) {
+            if(sensors[i] < THRESHOLD) {
+                boolSensorData[i] = true;
+            } else {
+                boolSensorData[i] = false;
+            }
+        }
+
+        // IIOO
+        if(boolSensorData[0] && boolSensorData[1] && !boolSensorData[2] && !boolSensorData[3]) {
+            turnLeft();
+        }
+        // OOII
+        else if(!boolSensorData[0] && !boolSensorData[1] && boolSensorData[2] && boolSensorData[3]) {
+            turnRight();
+        }
+        // OIIO
+        else if(!boolSensorData[0] && boolSensorData[1] && boolSensorData[2] && !boolSensorData[3]) {
+            goStraight();
+        }
+        
         if(myController->b()) {
             Console.println("button b pressed - exiting to main");
             break;
